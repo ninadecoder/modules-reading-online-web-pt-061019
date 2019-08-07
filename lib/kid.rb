9d4d4a83d1e04332f9055bca5f323fde91456978
Nina Kazzim <kazzim@icloud.com>
2019-08-07 04:25:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,16 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
+class Dancer
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
+
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
